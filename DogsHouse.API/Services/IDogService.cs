a0df86f66051c280f9a5cbd21fce542123f9719e
oleksandr <alexkostrytsia@gmail.com>
2024-10-30 14:29:16 +0200

    [refactor] : merge sort logic to list

--- DogsHouse.API/Services/IDogService.cs
+++ DogsHouse.API/Services/IDogService.cs
@@ -8,7 +8,5 @@ public interface IDogService
 
     public Task AddAsync(DogAddDto? dog, CancellationToken cancellationToken);
 
-    public Task<IEnumerable<DogDto>> ListAsync(CancellationToken cancellationToken);
-
-    public Task<IEnumerable<DogDto>> Sort(string attribute, string order);
+    public Task<IEnumerable<DogDto>> ListAsync(string? attribute, string? order,CancellationToken cancellationToken);
 }
