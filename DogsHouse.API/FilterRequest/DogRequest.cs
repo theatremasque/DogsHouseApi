235521f8c7f2d0e48ef9d51ab0bc11f85e425203
oleksandr <alexkostrytsia@gmail.com>
2024-10-30 14:44:35 +0200

    [feature] : create dog request model, that describe some conditionals of getting list of dogs, for sorting and pagination

---
+++ DogsHouse.API/FilterRequest/DogRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DogsHouse.API.FilterRequest;
+
+public class DogRequest
+{
+    public string? Attribute { get; set; }
+    public string? Order { get; set; }
+    public int? PageNumber { get; set; }
+    public int? PageSize { get; set; }
+}
