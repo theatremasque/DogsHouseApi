d00dcda085072c1b56113309592a40ab2e5566e5
oleksandr <alexkostrytsia@gmail.com>
2024-10-30 13:27:00 +0200

    [feature] : add method to interface Add()

--- DogsHouse.API/Services/IDogService.cs
+++ DogsHouse.API/Services/IDogService.cs
@@ -1,6 +1,10 @@
-﻿namespace DogsHouse.API.Services;
+﻿using DogsHouse.API.Dtos;
+
+namespace DogsHouse.API.Services;
 
 public interface IDogService
 {
     public string Ping();
+
+    public Task Add(DogAddDto? dog, CancellationToken cancellationToken);
 }
