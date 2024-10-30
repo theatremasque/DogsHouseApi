96b895cac15325ca0ae093849b61799418f1fc72
oleksandr <alexkostrytsia@gmail.com>
2024-10-30 13:25:24 +0200

    [feature] : add data transfer object for action that add dog to table

---
+++ DogsHouse.API/Dtos/DogAddDto.cs
@@ -0,0 +1,12 @@
+﻿namespace DogsHouse.API.Dtos;
+
+public class DogAddDto
+{
+    public string Name { get; set; } = null!;
+    
+    public string Color { get; set; } = null!;
+
+    public int TailLength { get; set; }
+
+    public int Weight { get; set; }
+}
