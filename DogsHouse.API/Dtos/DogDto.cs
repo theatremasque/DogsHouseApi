1af93c796efa77a73966ced1f962e8f695a30024
oleksandr <alexkostrytsia@gmail.com>
2024-10-30 13:29:44 +0200

    [feature] : add dto for get dog, or list of dogs

---
+++ DogsHouse.API/Dtos/DogDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DogsHouse.API.Dtos;
+
+public class DogDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Color { get; set; } = null!;
+    public int TailLength { get; set; }
+    public int Weight { get; set; }
+}
