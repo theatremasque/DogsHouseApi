a8c48139a723011bd9346f0e1b6418901c8a4461
oleksandr <alexkostrytsia@gmail.com>
2024-10-30 12:31:30 +0200

    [feature] : add ctx

---
+++ DogsHouse.API/Infrastructure/DogDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DogsHouse.API.Infrastructure;
+
+public class DogDbContext : DbContext
+{
+    public DogDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
