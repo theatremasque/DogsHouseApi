b78964de46d466c660ad2b8db47bdabdd422e129
oleksandr <alexkostrytsia@gmail.com>
2024-10-30 13:25:54 +0200

    [feature] : realize service action that add dog

--- DogsHouse.API/Services/DogService.cs
+++ DogsHouse.API/Services/DogService.cs
@@ -1,11 +1,40 @@
-﻿namespace DogsHouse.API.Services;
+﻿using DogsHouse.API.Dtos;
+using DogsHouse.API.Entities;
+using DogsHouse.API.Infrastructure;
+
+namespace DogsHouse.API.Services;
 
 public class DogService : IDogService
 {
+    private readonly PetsDbContext _ctx;
+
+    public DogService(PetsDbContext ctx)
+    {
+        _ctx = ctx;
+    }
+
     public string Ping()
     {
         var message = "Dogshouseservice.Version1.0.1";
 
         return message;
+    }
+
+    public async Task Add(DogAddDto? dog, CancellationToken cancellationToken)
+    {
+        if (dog != null)
+        {
+            var entity = new Dog()
+            {
+                Name = dog.Name,
+                Color = dog.Color,
+                TailLength = dog.TailLength,
+                Weight = dog.Weight
+            };
+
+            _ctx.Dogs.Add(entity);
+
+            await _ctx.SaveChangesAsync(cancellationToken);
+        }
     }
 }
