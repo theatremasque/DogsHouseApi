33f638322b3a10423dace96957c9a07544d34923
oleksandr <alexkostrytsia@gmail.com>
2024-10-30 13:11:27 +0200

    [refactor] : change logic, get message from via service that work with dogs.

--- DogsHouse.API/Controllers/DogController.cs
+++ DogsHouse.API/Controllers/DogController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DogsHouse.API.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DogsHouse.API.Controllers;
 
@@ -6,10 +7,17 @@ namespace DogsHouse.API.Controllers;
 [Route("api/[controller]/[action]")]
 public class DogController : ControllerBase
 {
+    private readonly IDogService _dogService;
+
+    public DogController(IDogService dogService)
+    {
+        _dogService = dogService;
+    }
+
     [HttpGet]
     public async Task<IActionResult> Ping()
     {
-        var message = "Dogshouseservice.Version1.0.1";
+        var message = _dogService.Ping();
         
         return Ok(message);
     }
