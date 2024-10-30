be65d9bdb832ef763b2011338db650878264d9e4
oleksandr <alexkostrytsia@gmail.com>
2024-10-30 13:05:30 +0200

    [feature] : add DogService.cs that implements IDogService and realize actions

---
+++ DogsHouse.API/Services/DogService.cs
@@ -0,0 +1,11 @@
+﻿namespace DogsHouse.API.Services;
+
+public class DogService : IDogService
+{
+    public string Ping()
+    {
+        var message = "Dogshouseservice.Version1.0.1";
+
+        return message;
+    }
+}
