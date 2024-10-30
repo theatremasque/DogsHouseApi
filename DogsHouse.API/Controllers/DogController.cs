256f961294d988154aa97ad2e36450fad25406d3
oleksandr <alexkostrytsia@gmail.com>
2024-10-30 12:21:22 +0200

    [feature] : creation

---
+++ DogsHouse.API/Controllers/DogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DogsHouse.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class DogController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Ping()
+    {
+        var message = "Dogshouseservice.Version1.0.1";
+        
+        return Ok(message);
+    }
+}
