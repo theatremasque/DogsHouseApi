1f487eda123e0f7bd3cfdad18e2448bbfec0cda9
oleksandr <alexkostrytsia@gmail.com>
2024-10-30 12:52:05 +0200

    [feature] : add DbSet property, that represent relational to Dogs table

--- DogsHouse.API/Infrastructure/PetsDbContext.cs
+++ DogsHouse.API/Infrastructure/PetsDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DogsHouse.API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DogsHouse.API.Infrastructure;
 
 public class PetsDbContext : DbContext
 {
+    public DbSet<Dog> Dogs { get; set; }
+    
     public PetsDbContext(DbContextOptions options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
