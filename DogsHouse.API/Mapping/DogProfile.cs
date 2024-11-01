2274362a380f8bca24b0d9ab81c1f4fa9278acdc
oleksandr <alexkostrytsia@gmail.com>
2024-11-01 11:32:59 +0200

    [feature] : create dog profile

---
+++ DogsHouse.API/Mapping/DogProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DogsHouse.API.Dtos;
+using DogsHouse.API.Entities;
+
+namespace DogsHouse.API.Mapping;
+
+public class DogProfile : Profile
+{
+    public DogProfile()
+    {
+        CreateMap<DogAddDto, Dog>();
+
+        CreateProjection<Dog, DogDto>();
+    }
+}
