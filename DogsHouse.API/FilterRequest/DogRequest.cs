c7941d57351385ebb5cf74aa06f440f5312fc6d8
oleksandr <alexkostrytsia@gmail.com>
2024-11-01 11:35:18 +0200

    [clean up] : remove unsed class

--- DogsHouse.API/FilterRequest/DogRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DogsHouse.API.FilterRequest;
-
-public class DogRequest
-{
-    public string? Attribute { get; set; }
-    public string? Order { get; set; }
-    public int? PageNumber { get; set; }
-    public int? PageSize { get; set; }
-}
