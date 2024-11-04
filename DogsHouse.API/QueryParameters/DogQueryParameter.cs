64f8b4d1929b09a07c784b16cfe15862102a34f9
oleksandr <alexkostrytsia@gmail.com>
2024-11-04 11:26:10 +0200

    [feature] : add class that describes query for action List()

---
+++ DogsHouse.API/QueryParameters/DogQueryParameter.cs
@@ -0,0 +1,9 @@
+﻿namespace DogsHouse.API.QueryParameters;
+
+public class DogQueryParameter
+{
+    public string? Attribute { get; set; }
+    public string? Order { get; set; }
+    public int? PageNumber { get; set; }
+    public int? PageSize { get; set; }
+}
