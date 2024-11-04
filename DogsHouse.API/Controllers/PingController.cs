4b1b71087379b9d082419894444a9410846ae68f
oleksandr <alexkostrytsia@gmail.com>
2024-11-04 11:18:33 +0200

    [feature] : add ping controller with GET action PING()

---
+++ DogsHouse.API/Controllers/PingController.cs
@@ -0,0 +1,26 @@
+﻿using DogsHouse.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace DogsHouse.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PingController : ControllerBase
+{
+    private readonly IPingService _pingService;
+
+    public PingController(IPingService pingService)
+    {
+        _pingService = pingService;
+    }
+
+    [EnableRateLimiting("fixed")]
+    [HttpGet]
+    public IActionResult Ping()
+    {
+        var message = _pingService.Ping();
+
+        return Ok(message);
+    }
+}
